9ae4787068987b103956df75d68599da45ba3011
u1_mas <mtakuya1022@gmail.com>
2024-06-20 11:01:07 +0900

    add dotenv allowEmptyValues

--- dev.ts
+++ dev.ts
@@ -1,8 +1,18 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
 import dev from "$fresh/dev.ts";
+import { loadSync } from "$std/dotenv/mod.ts";
 import config from "./fresh.config.ts";
 
-import "$std/dotenv/load.ts";
+// import "$std/dotenv/load.ts";
+
+if (!(Deno.readTextFileSync instanceof Function)) {
+    // Avoid errors that occur in deno deploy: https://github.com/denoland/deno_std/issues/1957
+    console.warn(
+        `Deno.readTextFileSync is not a function: No .env data was read.`,
+    );
+} else {
+    loadSync({ export: true, allowEmptyValues: true });
+}
 
 await dev(import.meta.url, "./main.ts", config);
--- main.ts
+++ main.ts
@@ -4,7 +4,17 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
-import "$std/dotenv/load.ts";
+import { loadSync } from "$std/dotenv/mod.ts";
+
+// import "$std/dotenv/load.ts";
+if (!(Deno.readTextFileSync instanceof Function)) {
+    // Avoid errors that occur in deno deploy: https://github.com/denoland/deno_std/issues/1957
+    console.warn(
+        `Deno.readTextFileSync is not a function: No .env data was read.`,
+    );
+} else {
+    loadSync({ export: true, allowEmptyValues: true });
+}
 
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
