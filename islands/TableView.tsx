2a7fdb392667c743fda8d56e85e44f8cd28f5b8a
u1_mas <mtakuya1022@gmail.com>
2024-06-26 21:29:45 +0900

    add diff by day

--- islands/TableView.tsx
+++ islands/TableView.tsx
@@ -11,33 +11,38 @@ export default function TableView(
   }
   console.log({ tableData: tableData.value });
   return (
-    <div className="whitespace-nowrap overflow-auto w-[100%] max-h-[500px] mt-[100px]">
-      <table className="table-auto">
-        <thead className="sticky top-0 z-10 ">
-          <tr className="bg-gray-200">
-            <th className="sticky left-0  bg-white border ">
-              {tableData.value[0][0]}
-            </th>
-            {tableData.value[0].slice(1).map((h) => (
-              <th className="px-4 py-2 border">{h}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {tableData.value.slice(1).map((r, i) => (
-            <tr key={i}>
-              <td className="px-4 py-2 sticky left-0 z-[2] bg-slate-100 border">
-                {r[0]} {/* 日付 */}
-              </td>
-              {r.slice(1).map((d) => (
-                <td className="px-4 py-2 border">
-                  <span dangerouslySetInnerHTML={{ __html: d }}></span>
-                </td>
+    <div className="my-[10px]">
+      <div className="my-[10px]">
+        総対戦数 / 総合勝率 (変化率) / 日別対戦数 (勝 : 負)
+      </div>
+      <div className="whitespace-nowrap overflow-auto w-[100%] max-h-[500px]">
+        <table className="table-auto">
+          <thead className="sticky top-0 z-10 ">
+            <tr className="bg-gray-200">
+              <th className="sticky left-0  bg-white border ">
+                {tableData.value[0][0]}
+              </th>
+              {tableData.value[0].slice(1).map((h) => (
+                <th className="px-4 py-2 border">{h}</th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {tableData.value.slice(1).map((r, i) => (
+              <tr key={i}>
+                <td className="px-4 py-2 sticky left-0 z-[2] bg-slate-100 border">
+                  {r[0]} {/* 日付 */}
+                </td>
+                {r.slice(1).map((d) => (
+                  <td className="px-4 py-2 border">
+                    <span dangerouslySetInnerHTML={{ __html: d }}></span>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
